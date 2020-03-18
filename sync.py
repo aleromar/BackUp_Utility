ba684d19a094092fbb4a0e6f27174c5ee8c7293c
alero <aleromar@gmail.com>
2020-03-18 15:46:15 +0000

    Adding some more functionality

--- sync.py
+++ sync.py
@@ -1,18 +1,18 @@
 import os
 import shutil
+import datetime
 
-#FOLDER_ORIGIN = r'C:\Users\alero\Documents\Viajes'
-#FOLDER_DEST   = r'C:\Users\alero\Documents\Viajes2'
 FOLDER_ORIGIN = r'D:'
-FOLDER_BACKUP = r'E:'
+FOLDER_BACKUP = os.getcwd()
+FOLDERS_TO_SYNC = ['alejandro','Documents']
 
-def findDifferences(pathOrigin, pathDest, fileList,folderList):
+def findDifferences(fError,pathOrigin, pathDest, fileList,folderList):
     createNewPath = lambda path,name: path+'\\'+name
     try:
         for name in os.listdir(pathOrigin):
             if os.path.isdir(createNewPath(pathOrigin,name)):
                 if name in os.listdir(pathDest):
-                    findDifferences(createNewPath(pathOrigin,name),createNewPath(pathDest,name), fileList,folderList)
+                    findDifferences(fError,createNewPath(pathOrigin,name),createNewPath(pathDest,name), fileList,folderList)
                 else:
                     folderList.append(createNewPath(pathOrigin,name))
             else:
@@ -21,47 +21,53 @@ def findDifferences(pathOrigin, pathDest, fileList,folderList):
                 try:
                     fileSize = os.stat(createNewPath(pathOrigin,name)).st_size
                     if  fileSize != os.stat(createNewPath(pathDest,name)).st_size:
-                        print('File size is different for {}'.format(createNewPath(pathDest,name)))
+                        fError.write('File size is different for {}\n'.format(createNewPath(pathDest,name)))
                 except:
-                    print('Problems when getting file size for file {}'.format(createNewPath(pathDest,name)))
+                    fError.write('Problems when getting file size for file {}\n'.format(createNewPath(pathDest,name)))
     except:
-        print('\n')
-        print('***ERROR occurred while trying to work on path {}'.format(pathOrigin))
-        print('\n')
+        fError.write('\n')
+        fError.write('ERROR occurred while trying to work on path {}\n'.format(pathOrigin))
+        fError.write('\n')
 
-def CreateResultFile(misFoldO,misFileO,misFoldB,misFileB):
+def CreateResultFile(f,misFoldO,misFileO,misFoldB,misFileB,orPath,bckPath):
+    fcPrefix = '    '
     def printList(thisList,filehandler,prefix):
         for item in thisList:
-            filehandler.write(prefix+item+'\n')
+            filehandler.write(fcPrefix+prefix+item+'\n')
+
+    def templateCode(f,keyWord, thisList, isBackUp):
+        backupstring = lambda isBackUp : 'back-up' if isBackUp else 'origin'
+        if(len(thisList) == 0):
+            f.write('{}    There are no detected new {} in {}. {} are in SYNC!\n'.format(fcPrefix,keyWord,backupstring(not isBackUp),keyWord.title()))
+        else:
+            f.write('{}    There are a total of {} new {} NOT present in the {}\n'.format(fcPrefix,len(thisList),keyWord,backupstring(isBackUp)))
+            printList(thisList,f,'    - ')
+            f.write('\n')
+    isBackUp = True
+    isOrigin = False
+    f.write('{}The following will need to be COPIED from {} to the backup drive: {}\n'.format(fcPrefix,orPath,bckPath))
+    templateCode(f,'folders',misFoldB,isBackUp)
+    templateCode(f,'files',misFileB,isBackUp)
+    f.write('\n\n')
+    f.write('{}The following will need to be REMOVED in the backup drive: {}\n'.format(fcPrefix,bckPath))
+    templateCode(f,'folders',misFoldO,isOrigin)
+    templateCode(f,'files',misFileO,isOrigin)
+
+with open('errorLog.txt','w') as fError:
+    enterTime = lambda handle: handle.write('Sync script run on {}\n\n'.format(datetime.datetime.now()))
     with open('results.txt','w') as f:
-        def templateCode(f,keyWord, thisList, isBackUp):
-            backupstring = lambda isBackUp : 'back-up' if isBackUp else 'origin'
-            if(len(thisList) == 0):
-                f.write('There are no detected new {} in {}. {} are in SYNC!\n'.format(keyWord,backupstring(not isBackUp),keyWord.title()))
-            else:
-                f.write('There are a total of {} new {} NOT present in the {}\n'.format(len(thisList),keyWord,backupstring(isBackUp)))
-                printList(thisList,f,'    - ')
-                f.write('\n')
-        isBackUp = True
-        isOrigin = False
-        f.write('**************************************\n')
-        f.write('The following will need to be COPIED from {} to the backup drive: {}\n'.format(FOLDER_ORIGIN,FOLDER_BACKUP))
-        f.write('**************************************\n')
-        templateCode(f,'folders',misFoldB,isBackUp)
-        templateCode(f,'files',misFileB,isBackUp)
-        f.write('\n\n**************************************\n')
-        f.write('The following will need to be REMOVED in the backup drive: {}\n'.format(FOLDER_BACKUP))
-        f.write('**************************************\n')
-        templateCode(f,'folders',misFoldO,isOrigin)
-        templateCode(f,'files',misFileO,isOrigin)
-
-
-missingFilesInO = []
-missingFoldersInO = []
-missingFilesInB = []
-missingFoldersInB = []
-
-findDifferences(FOLDER_ORIGIN,FOLDER_BACKUP,missingFilesInB,missingFoldersInB)
-findDifferences(FOLDER_BACKUP,FOLDER_ORIGIN,missingFilesInO,missingFoldersInO)
-
-CreateResultFile(missingFoldersInO,missingFilesInO,missingFoldersInB,missingFilesInB)
+        enterTime(f)
+        enterTime(fError)
+        for foldertosync in FOLDERS_TO_SYNC:
+            f.write('**************************************\n')
+            f.write('Results {}\n'.format(foldertosync.upper()))
+            f.write('**************************************\n')
+            missingFilesInO = []
+            missingFoldersInO = []
+            missingFilesInB = []
+            missingFoldersInB = []
+            createNewPath = lambda path: path+'\\'+foldertosync
+            findDifferences(fError,createNewPath(FOLDER_ORIGIN),createNewPath(FOLDER_BACKUP),missingFilesInB,missingFoldersInB)
+            findDifferences(fError,createNewPath(FOLDER_BACKUP),createNewPath(FOLDER_ORIGIN),missingFilesInO,missingFoldersInO)
+            CreateResultFile(f,missingFoldersInO,missingFilesInO,missingFoldersInB,missingFilesInB,createNewPath(FOLDER_ORIGIN),createNewPath(FOLDER_BACKUP))
+            f.write('\n\n')
